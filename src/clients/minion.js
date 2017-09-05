2219ba4eb5d1aa7934dd8eda265764696a4abe6a
Roman Pushkov <pushkovroman@me.com>
2017-09-05 11:25:42 +0300

    spacing

--- src/clients/minion.js
+++ src/clients/minion.js
@@ -11,36 +11,36 @@ const getTask = (client, firstCall = false) => {
     console.log(highlight(`Minion ${process.pid} is now processing tasks`));
   }
   client
-  .multi()
-  .exists('master') // check for a master
-  .rpop('tasks')    // fetch a task
-  .exec((error, data) => {
-    if (error) {
-      throw new Error(error);
-    }
-
-    // predicates
-    const masterPresent = data[0] === 1;
-    const task = data[1];
-
-    // avoid null messages
-    if (task) {
-      // error generator
-      if (isErroneous()) {
-        console.log(highlight(`Minion ${process.pid} has found an error in ${task}`));
-        client.rpush('errors', task);
-      } else {
-        console.log(`Task done by ${process.pid}: ${task}`);
+    .multi()
+    .exists('master') // check for a master
+    .rpop('tasks')    // fetch a task
+    .exec((error, data) => {
+      if (error) {
+        throw new Error(error);
       }
-    }
 
-    // promote to master if no master exists
-    if (masterPresent) {
-      setTimeout(getTask, 500, client);
-    } else {
-      master(client);
-    }
-  });
+      // predicates
+      const masterPresent = data[0] === 1;
+      const task = data[1];
+
+      // avoid null messages
+      if (task) {
+        // error generator
+        if (isErroneous()) {
+          console.log(highlight(`Minion ${process.pid} has found an error in ${task}`));
+          client.rpush('errors', task);
+        } else {
+          console.log(`Task done by ${process.pid}: ${task}`);
+        }
+      }
+
+      // promote to master if no master exists
+      if (masterPresent) {
+        setTimeout(getTask, 500, client);
+      } else {
+        master(client);
+      }
+    });
 };
 
 export default client => getTask(client, true);
